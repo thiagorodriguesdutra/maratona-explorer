7bbe7b752891465b42b9d170bad96af840052e60
Thiago Dutra <thiagodutra@MacBook-Pro-de-Thiago.local>
2022-06-08 10:49:16 -0300

    create main function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
-// Em breve teremos código aqui...
+const questionElement = document.querySelector('.question')
+const answerElement = document.querySelector('.answer')
+const divElement = document.querySelector('#interaction')
+const inputElement = document.querySelector('input')
+const button = document.querySelector('button')
+
+const answers = [
+    "Não entendi, seja mais claro",
+    "Sim!",
+    "Talvez?",
+    "Não sou capaz de opinar",
+    "Com certeza!",
+    "Se concentre, pergunte novamente",
+    "Não sou obrigado a responder"   
+]
+
+function askAQuestion () {
+    const inputValue = inputElement.value
+    divElement.classList.remove('hide')
+    questionElement.innerText = inputValue
+    answerElement.innerText = answers[Math.floor(Math.random()*answers.length)]
+    inputElement.value = ""
+    setTimeout(() => {
+        divElement.classList.add('hide');
+    }, 1500)
+}
+
+button.addEventListener('click', askAQuestion)
+
